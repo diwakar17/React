840ae8fc6480df267ce64bff5c73e038e1f50513
Your Name <you@example.com>
2018-11-08 21:22:10 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,67 +4,88 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 /*var createClass = require('create-react-class');*/
 
-class Images extends React.Component {
-  render() {
+
+const iStyle = {
+  height:200,
+  width:270,
+}
+//component for carousel
+class Caarousel extends React.Component {
+  render () {
     return(
-      <div>
-      <Carousel>
+      <Carousel style={{width:20}}>
              <div>
                  <img src="https://s3-us-west-2.amazonaws.com/crush-assets/slider-1.png" alt="slider"/>
-                 <p>Legend 1</p>
              </div>
              <div>
                  <img src="https://s3-us-west-2.amazonaws.com/crush-assets/slider-2.png" alt="slider"/>
-                 <p>Legend 2</p>
              </div>
              <div>
                  <img src="https://s3-us-west-2.amazonaws.com/crush-assets/slider-3.png" alt="slider" />
-                 <p>Legend 3</p>
              </div>
          </Carousel>
-          <div style={{display:'flex', flexWrap:"wrap", justifyContent:"space-around", paddingLeft:200, paddingRight:200}}>
+    );
+  }
+}
+
+//component for color
+class Images extends React.Component {
+  render() {
+    return(
+
+          <div style={{display:'flex', flexWrap:"wrap", justifyContent:"space-between", paddingLeft:150, paddingRight:150}}>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={iStyle} />
               <p>Product 1</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={iStyle} />
               <p>Product 2</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/026-brn" alt="Product" style={iStyle} />
               <p>Product 3</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={iStyle} />
               <p>Product 1</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={iStyle} />
               <p>Product 2</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/anytime-brown" alt="Product" style={iStyle} />
               <p>Product 3</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={iStyle} />
               <p>Product 1</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={iStyle} />
               <p>Product 2</p>
             </div>
             <div style={{display:'flex', alignItems:'center', flexDirection:'column'}}>
-              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={{height:200, width:270}} />
+              <img src="https://s3-ap-southeast-1.amazonaws.com/asianapp/mr-132-n-blu" alt="Product" style={iStyle} />
               <p>Product 3</p>
             </div>
-          </div>
           </div>
     );
   }
 }
 
+class Show extends React.Component {
+  render() {
+    return(
+      <div className="text-center">
+      <Caarousel />
+      <Images />
+      </div>
+    );
+  }
+}
+//rendering dom on same component's page this can be done on a different page and can simply export this one
 ReactDOM.render(
-  <Images />,
+  <Show />,
   document.getElementById('root'));
